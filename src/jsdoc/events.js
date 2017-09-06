cb29a027a0acdc941d5f8ab35da636ac9b34ffed
jy95 <jacques.yakoub@gmail.com>
2017-09-06 19:30:25 +0200

    Docs: separate jsdoc into another files
    Too documentation in source code kills the documentation
    Create a jsdoc folder in src so that we can store futher doc

---
+++ src/jsdoc/events.js
@@ -0,0 +1,17 @@
+// events
+
+/**
+ * missing_parameter event
+ * @event TorrentLibrary#missing_parameter
+ * @type {object}
+ * @property {string} functionName - Indicates the function name when the error occurs.
+ */
+
+/**
+ * Error event
+ * @event TorrentLibrary#error
+ * @type {object}
+ * @property {string} functionName - Indicates the function name when the error occurs.
+ * @property {object} error - The error object
+ */
+
