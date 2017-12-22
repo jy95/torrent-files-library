5ec79c595ae1816c353a1da1f5a16220a9aca527
jy95 <jacques.yakoub@gmail.com>
2017-12-23 00:46:15 +0100

    Chore: Update lock file

---
+++ release-rules.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    tag: 'Feat',
+    release: 'minor',
+  },
+  {
+    tag: 'Refactor',
+    release: 'patch',
+  },
+  {
+    tag: 'Fix',
+    release: 'patch',
+  },
+  {
+    tag: 'Perf',
+    release: 'patch',
+  },
+  {
+    tag: 'Breaking',
+    release: 'major',
+  },
+];
