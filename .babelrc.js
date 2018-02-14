6df2740d2e2271169cd736802a147cccf0882e65
jy95 <jacques.yakoub@gmail.com>
2018-02-15 00:48:51 +0100

    :zap: Perf: use Babel/minify to make a better build

--- .babelrc.js
+++ .babelrc.js
@@ -1,14 +1,24 @@
 let env = process.env.BABEL_ENV || process.env.NODE_ENV;
-let presets = ["@babel/preset-env"];
+let presets = [];
 let plugins = ["@babel/plugin-proposal-object-rest-spread"];
 let ignore = ["src/docs/**/*.js"];
 let comments = false;
 
 // custom settings for prod/test ; for example babel-istanbul-plugin , etc.
-// ./fix-coverage/arrow-function-coverage-fix.js : Because some odd issue for istanbul/nyc
-if (env === 'test'){
-  plugins.push.apply(plugins, ["./fix-coverage/arrow-function-coverage-fix.js", "babel-plugin-istanbul"]);
+switch (env){
+  case 'test':
+    // ./fix-coverage/arrow-function-coverage-fix.js : Because some odd issue for istanbul/nyc
+    plugins.push.apply(plugins, ["babel-plugin-istanbul"]);
+    break;
+
+  case 'production':
+    // workaround : https://github.com/babel/minify/issues/790#issuecomment-365750066
+    presets.push.apply(presets, [['minify', { removeUndefined: false }]] );
+    break;
 }
+
+// default preset
+presets.push.apply(presets, ["@babel/preset-env"]);
 
 module.exports = {
   presets: presets,
